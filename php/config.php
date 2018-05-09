4f640dbe15796b2c686a8063f13ac482aee2f162
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2018-05-09 17:20:32 +0300

    Update config.php
    Updated for Dijkstra server!

--- php/config.php
+++ php/config.php
@@ -1,13 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "project";
+$dbservername = "dijkstra.ug.bcc.bilkent.edu.tr";
+$dbusername = "ulas.is";
+$dbpassword = "7fq5bg09";
+$dbname = "ulas_is";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
 
 if(!$conn){
   echo "<script>console.log('Connection is failed: " . mysqli_connect_error() . "!');</script>";
 }
-echo "<script>console.log('Connection is successful!');</script>";
+else{
+  echo "<script>console.log('Connection is successful!');</script>";
+}
 ?>
