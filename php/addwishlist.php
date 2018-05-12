53d02e7c4957e472b5ffd10dc0b65630a6eef5dd
ulasis <32201548+ulasis@users.noreply.github.com>
2018-05-12 14:45:55 +0300

    Add files via upload

---
+++ php/addwishlist.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include_once 'config.php';
+    if(isset($_SESSION['UserID'])){
+      $user_id = $_SESSION['UserID'];
+      $game_id = $_POST['GameID'];
+      $check = "SELECT * FROM WishList WHERE UserUserID='$user_id' AND GameGameID='$game_id'";
+      $result = mysqli_query($conn, $check);
+      $result_check = mysqli_num_rows($result);
+      if($result_check == 0){
+      $sql = "INSERT INTO WishList (UserUserID, GameGameID) VALUES('$user_id', '$game_id')";
+      $result = mysqli_query($conn, $sql);
+      header("Location: ../~$dbusername/game.php?GameID=$game_id");
+      }
+      else{
+	header("Location: ../~$dbusername/game.php?GameID=$game_id");
+      }
+   }
+   else{
+    header("Location: ../~$dbusername/game.php?GameID=$game_id");
+    exit();
+   }
+?>
---
+++ php/buy.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include_once 'config.php';
+    if(isset($_SESSION['UserID'])){
+     if(isset($_POST['GameID'])){
+      $user_id = $_SESSION['UserID'];
+      $game_id = $_POST['GameID'];
+      $sql = "SELECT Balance FROM User WHERE UserID='$user_id'";
+      $result = mysqli_query($conn, $sql);
+      $row = mysqli_fetch_assoc($result);
+
+      $balance = $row['Balance'];
+      $sql = "SELECT Price FROM Game WHERE GameID='$game_id'";
+      $result = mysqli_query($conn, $sql);
+      $row = mysqli_fetch_assoc($result);
+      $price = $row['Price'];
+      echo $price;
+      if($price <= $balance){
+        $sql = "INSERT INTO Buy (UserUserID, GameGameID) VALUES('$user_id', '$game_id')";
+        $result = mysqli_query($conn, $sql);
+	$newbalance = ((double)$balance) - ((double)$price);
+	$sql = "UPDATE TABLE User SET Balance = '$newbalance' WHERE UserID = '$user_id'";
+	$result = mysqli_query($conn, $sql);
+        header("Location: ../~$dbusername/game.php?GameID=$game_id");
+      }
+      else{
+	header("Location: ../~$dbusername/game.php?GameID=$game_id");
+      }
+     }
+     else if(isset($_POST['GameIDCard'])){
+      $user_id = $_SESSION['UserID'];
+      $game_id = $_POST['GameIDCard'];
+      $sql = "SELECT CardNo, Cvv FROM User WHERE UserID='$user_id'";
+      $result = mysqli_query($conn, $sql);
+      $row = mysql_fetch_assoc($result);
+      if(is_null($row['Cvv']) || is_null($row['CardNo']))	
+      	header("Location: ../~$dbusername/store.php");
+      else
+	header("Location: ../~$dbusername/game.php?GameID=$game_id");
+     }
+   }
+   else{
+    header("Location: ../~$dbusername/game.php?GameID=$game_id");
+    exit();
+   }
+?>
