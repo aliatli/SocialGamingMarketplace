86c8031697f79e3748b258c50f6be1c6185717ad
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2018-05-15 10:56:52 +0300

    Update buy.php

--- php/buy.php
+++ php/buy.php
@@ -18,6 +18,22 @@ if(isset($_SESSION['UserID'])){
     if($row = mysqli_fetch_assoc($result)){
       $price = $row['Price'];
     }
+
+    if(isset($_POST['discount_balance'])){
+      $discount_balance = $_POST['discount_balance'];
+    }
+
+    $sql_discount = "SELECT * FROM Inventory, Discount_Card WHERE Discount_Card.ItemID = Inventory.ItemItemID AND Inventory.ItemItemID IN (SELECT Item.ItemID FROM Item, Discount_Card WHERE Item.ItemID = Discount_Card.ItemID AND Code = '$discount_balance' AND Item.GameGameID = $game_id);";
+    $result_discount = mysqli_query($conn, $sql_discount);
+    $result_discount_check = mysqli_num_rows($result_discount);
+
+    if($result_discount_check === 1){
+      if($row = mysqli_fetch_assoc($result_discount)){
+        $discount_rate = $row['DiscountRate'];
+	$price = $price / $discount_rate;
+	$discount_id = $row['ItemID'];
+      }
+    }
       
     if($price <= $balance){
       $sql = "INSERT INTO Buy (UserUserID, GameGameID) VALUES($user_id, $game_id)";
@@ -27,6 +43,12 @@ if(isset($_SESSION['UserID'])){
 
       $sql = "UPDATE User SET Balance = '$newbalance' WHERE UserID = $user_id";
       $result = mysqli_query($conn, $sql);
+
+$sql_delete = "DELETE FROM Item WHERE ItemID = $discount_id;";
+mysqli_query($conn, $sql_delete);
+$sql_delete = "DELETE FROM Inventory WHERE ItemItemID = $discount_id;";
+mysqli_query($conn, $sql_delete);
+
     }
   
     header("Location: ../~$dbusername/game.php?GameID=$game_id");
@@ -39,6 +61,20 @@ if(isset($_SESSION['UserID'])){
     $result = mysqli_query($conn, $sql);
     $row_no = mysqli_num_rows($result);
 
+    if(isset($_POST['discount_credit'])){
+      $discount_credit = $_POST['discount_credit'];
+    }
+
+    $sql_discount = "SELECT * FROM Inventory, Discount_Card WHERE Discount_Card.ItemID = Inventory.ItemItemID AND Inventory.ItemItemID IN (SELECT Item.ItemID FROM Item, Discount_Card WHERE Item.ItemID = Discount_Card.ItemID AND Code = '$discount_credit' AND Item.GameGameID = $game_id);";
+    $result_discount = mysqli_query($conn, $sql_discount);
+    $result_discount_check = mysqli_num_rows($result_discount);
+
+    if($result_discount_check === 1){
+      if($row = mysqli_fetch_assoc($result_discount)){
+	$discount_id = $row['ItemID'];
+      }
+    }
+
     if($row_no !== 0){
       if($row = mysqli_fetch_assoc($result)){
 	if(is_null($row['Cvv']) || is_null($row['CardNo']) || is_null($row[BillingAddress])){
@@ -48,6 +84,12 @@ if(isset($_SESSION['UserID'])){
 	else{
 	  $sql = "INSERT INTO Buy VALUES ($user_id, $game_id);";
           $result = mysqli_query($conn, $sql);
+
+$sql_delete = "DELETE FROM Item WHERE ItemID = $discount_id;";
+mysqli_query($conn, $sql_delete);
+$sql_delete = "DELETE FROM Inventory WHERE ItemItemID = $discount_id;";
+mysqli_query($conn, $sql_delete);
+
           header("Location: ../~$dbusername/game.php?GameID=$game_id");
           exit();
 	}
