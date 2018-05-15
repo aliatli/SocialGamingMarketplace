e6a75bd35d0039b031458cc770bffbb823c5cefb
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2018-05-15 11:20:10 +0300

    Update Database.java

--- db/Database.java
+++ db/Database.java
@@ -258,6 +258,12 @@ public class Database{
 					+ "ENGINE=innodb;";
 			stmt.executeUpdate(sql);
 			System.out.println("Created table Comment in the database");
+			
+			sql = "CREATE TRIGGER remove_wishlist"
+					+ "ON Buy AFTER INSERT "
+					+ "AS "
+					+ "DELETE FROM WishList WHERE Buy.UserUserID = WishList.UserUserID AND Buy.GameGameID = WishList.GameGameID";
+			stmt.executeUpdate(sql);
 
 		}catch (SQLException sqle) {
 			sqle.printStackTrace();
